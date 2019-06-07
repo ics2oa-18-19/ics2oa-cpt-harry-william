4fe407479ddb52ad8e1ba2c42502a0284c9ae7db
Harryxdy <47531653+Harryxdy@users.noreply.github.com>
2019-06-07 11:10:52 -0400

    Update cpt.py

--- master/cpt.py
+++ master/cpt.py
@@ -1,5 +1,4 @@
 import random
-import math
 import arcade
 
 SCREEN_WIDTH = 800
@@ -7,9 +6,9 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Kungfu Chef"
 
 
-class dropping_food:
+class Food:
     """
-    Each instance of this class represents a single snowflake.
+    Each instance of this class represents a single food.
     Based on drawing filled-circles.
     """
     def __init__(self):
@@ -18,86 +17,74 @@ class dropping_food:
 
     def reset_pos(self):
         # Reset flake to random position above screen
-        self.y = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT + 100)
-        self.x = random.randrange(SCREEN_WIDTH)
+        self.y = (SCREEN_HEIGHT + 20)
+        self.x = random.randrange(20,SCREEN_WIDTH-20)
 
 
 class MyGame(arcade.Window):
-    """ Main application class. """
+
 
     def __init__(self, width, height, title):
-        """
-        Initializer
-        :param width:
-        :param height:
-        """
         # Calls "__init__" of parent class (arcade.Window) to setup screen
         super().__init__(width, height, title)
 
         # Sprite lists
-        self.food_list = None
-
-    def start_dropping(self):
-        """ Set up snowfall and initialize variables. """
-        self.food_list = []
-
-        for i in range(1):
-            # Create snowflake instance
-            food = dropping_food()
-
-            # Randomly position snowflake
-            food.x = random.randrange(40,760)
-            food.y = (SCREEN_HEIGHT + 20)
-
-            # Set other variables for the snowflake
-            food.size = 20
-
-            food.speed = random.randrange(30,60)
-
-            # Add snowflake to snowflake list
-
-            self.food_list.append(food)
-
-        # Don't show the mouse pointer
-        self.set_mouse_visible(True)
-
-        # Set the background color
         arcade.set_background_color(arcade.color.COOL_GREY)
 
-    def on_draw(self):
-        """
-        Render the screen.
-        """
+        self.food_list = []
+        self.create_food()
 
-        # This command is necessary before drawing
+        self.round = 0
+        self.level = 1
+
+
+    def create_food(self):
+        """ Set up food and initialize variables. """
+
+        # Create food instance
+        food = Food()
+
+        # Randomly position foods
+        food.x = random.randrange(20,780)
+        food.y = (SCREEN_HEIGHT + 20)
+        food.size = 20
+        food.speed = (20)
+
+        self.food_list.append(food)
+
+
+
+    def on_draw(self):
         arcade.start_render()
 
-        # Draw the current position of each snowflake
+
+        # Draw the current position of each food
         for food in self.food_list:
             arcade.draw_circle_filled(food.x, food.y,
                                       food.size, arcade.color.ORANGE)
 
     def update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        """
 
-        # Animate all the snowflakes falling
+        # Animate all the foods falling
         for food in self.food_list:
             food.y -= food.speed * delta_time
-
-            # Check if snowflake has fallen below screen
+            food.speed += 0.05
             if food.y < -20:
                 food.reset_pos()
 
-            # Some math to make the snowflakes move side to side
+    def on_mouse_press(self, mouse_x: float, mouse_y: float, button: int, modifiers: int):
+
+        if (mouse_x, mouse_y) - (food.x, food.y) <= 20:
+            print("CLICKED")
+        else:
+            pass
+
 
 
 
 
 def main():
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    window.start_dropping()
     arcade.run()
 
 if __name__ == "__main__":
