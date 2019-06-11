3e20610a9c1a6fdeb31c9a68c2dd6fa11c345da9
666william <47435930+666william@users.noreply.github.com>
2019-06-11 11:00:55 -0400

    Update cpt.py

--- master/cpt.py
+++ master/cpt.py
@@ -1,10 +1,16 @@
 import random
 import arcade
 import math
+import os
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Kungfu Chef"
+tile_img1 = arcade.load_texture('/home/robuntu/xia/classwork/acade/5x7ft-vinyl-digital-chinese-kongfu-karate.jpg')
+tile_img2 = arcade.load_texture('/home/robuntu/xia/classwork/acade/image url.png')
 
 
 class Food:
@@ -57,30 +63,32 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,SCREEN_WIDTH, SCREEN_HEIGHT, tile_img1)
 
 
         # Draw the current position of each food
         for food in self.food_list:
-            arcade.draw_circle_filled(food.x, food.y,
-                                      food.size, arcade.color.ORANGE)
+            arcade.draw_texture_rectangle(food.x, food.y, 80, 60, tile_img2)
+
 
     def update(self, delta_time):
 
         # Animate all the foods falling
         for food in self.food_list:
             food.y -= food.speed * delta_time
-            food.speed += 0.1
             if food.y < -20:
                 food.reset_pos()
+                food.speed += 5
 
     def on_mouse_press(self, mouse_x: float, mouse_y: float, button: int, modifiers: int):
         for food in self.food_list:
             run = mouse_x - food.x
             rise = mouse_y - food.y
             distance = math.sqrt(run*run + rise*rise)
-            if distance <= 25:
+            if distance <= 36:
                 print("Clicked")
                 food.reset_pos()
+                food.speed += 5
             else:
                 pass
 
